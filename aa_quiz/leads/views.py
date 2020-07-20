03176ffd9a97eeebfc5f6feacda2b318659f5014
Monty Preston <monty.preston5@gmail.com>
2020-07-19 23:16:07 -0700

    Included Quiz Brief results in a dictionary on Lead Detail page.

--- aa_quiz/leads/views.py
+++ aa_quiz/leads/views.py
@@ -1,18 +1,26 @@
 from django.shortcuts import render, HttpResponse, redirect
 from .models import Lead
+import json
 
 # Create your views here.
 
 
 def index(request):
     context = {
-        'all_leads': Lead.objects.all()
+        'all_leads': Lead.objects.all(),
     }
     return render(request, 'leads/index.html', context)
 
 
 def lead_detail(request, id):
+
+    this_lead = Lead.objects.get(id=id)
+    this_brief = this_lead.quiz_brief
+
+    brief_dict = json.loads(this_brief)
+
     context = {
-        'lead': Lead.objects.get(id=id)
+        'lead': Lead.objects.get(id=id),
+        'brief': brief_dict,
     }
     return render(request, 'leads/lead_detail.html', context)
