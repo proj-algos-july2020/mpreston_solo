cbb503d5a8ed930f62a1ef3c90a6af5195cebd66
Monty Preston <monty.preston5@gmail.com>
2020-07-21 11:02:04 -0700

    Made minor updates to lead detail page. Messed around trying to come up with a clean way to present client brief data, but ultimately decided on just dropping in the data in json format. Ultimately were I to develop this CRM portion further, I'd present the brief in a much nicer way

--- aa_quiz/leads/models.py
+++ aa_quiz/leads/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from quiz.models import Persona
 import re
 
+
 # Create your models here.
 
 class LeadManager(models.Manager):
@@ -13,11 +14,11 @@ class LeadManager(models.Manager):
             errors['lname'] = "Last name must be at least 2 characters."
         EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
         if not EMAIL_REGEX.match(postData['contact-email']):
-            errors['email'] = "Please enter a valid email address"
-        if int(postData['contact-budget-min']) < 500:
-            errors['min_budget'] = "We require a minimum budget of $500 for this service."
-        if int(postData['contact-budget-max']) < int(postData['contact-budget-min']):
-            errors['max_budget'] = "Max budget must be greater than min budget."
+            errors['email'] = "Please enter a valid email address."
+        if not postData['contact-budget-min']:
+            errors['budget-min'] = "Please enter a minimum budget."
+        if not postData['contact-budget-max']:
+            errors['budget-max'] = "Please enter a maximum budget."
         return errors
 
 
@@ -29,8 +30,8 @@ class Lead(models.Model):
     budget_min = models.IntegerField()
     budget_max = models.IntegerField()
     intent_score = models.IntegerField()
-    persona_type = models.ForeignKey(Persona, related_name="has_leads", on_delete=models.CASCADE)
-    images = models.ImageField(null=True, upload_to='lead_uploads')
+    persona_type = models.ManyToManyField(Persona, related_name="has_leads", blank=True)
+    uploads = models.ImageField(null=True, upload_to='lead_uploads')
     quiz_brief = models.TextField()
     newsletter_opt_in = models.BooleanField()
     created_at = models.DateTimeField(auto_now_add=True)
--- aa_quiz/leads/views.py
+++ aa_quiz/leads/views.py
@@ -16,8 +16,13 @@ def lead_detail(request, id):
 
     this_lead = Lead.objects.get(id=id)
     this_brief = this_lead.quiz_brief
+    print(this_brief)
+
+    # formatted_brief = this_brief.split('')
+    # print(formatted_brief)
 
     brief_dict = json.loads(this_brief)
+    print(brief_dict)
 
     context = {
         'lead': Lead.objects.get(id=id),
