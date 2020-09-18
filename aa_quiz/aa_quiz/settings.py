d3e88347903e1f3ef75fa71ac40a93dd11bb9180
mpresto <60326593+mpresto@users.noreply.github.com>
2020-09-18 08:05:32 -0700

    Update settings.py
    Updated file to allow it to run if downloaded.

--- aa_quiz/aa_quiz/settings.py
+++ aa_quiz/aa_quiz/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
-from .secrets import *
+# from .secrets import *
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -21,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-
+SECRET_KEY = '8!7&7cosnao&9ywg=jbwz#^n8#ufn9pszq%kg)7!b_z^mfiztc'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
